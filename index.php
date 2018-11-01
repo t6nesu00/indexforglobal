c791a83d84ad68a52337956c6798170a466f01be
t6nesu00 <hlw.zigri@gamil.com>
2018-11-02 01:23:07 +0200

    some modification in nav bar and footer part

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 			      <!--<a class="nav-item nav-link" href="#">DESTINATIONS</a>-->
 			      <li class="nav-item dropdown">
 				        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				          Dropdown
+				          DESTINATIONS
 				        </a>
 				        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 				          <a class="dropdown-item" href="#">Destination 1</a>
